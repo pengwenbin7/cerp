aa55c1f8b8af0f18c41764bf521a72800dbcf615
pengwenbin7 <pengwenbin7@126.com>
2016-05-13 18:00:54 +0800

    160513

--- json/builder.c
+++ json/builder.c
@@ -5,29 +5,37 @@
 
 int main(int argc, char *argv[])
 {
-  JsonBuilder *builder = json_builder_new ();
+  JsonBuilder *builder = json_builder_new();
 
-  json_builder_begin_object (builder);
+  json_builder_begin_object(builder);
 
-  json_builder_set_member_name (builder, "string");
-  json_builder_add_string_value (builder, "你好，中国");
+  json_builder_set_member_name(builder, "string");
+  json_builder_add_string_value(builder, "你好，中国");
+  
+  json_builder_set_member_name(builder, "obj");
+  json_builder_begin_object(builder);
+  json_builder_set_member_name(builder, "cn");
+  json_builder_add_string_value(builder, "中国");
+  json_builder_set_member_name(builder, "us");
+  json_builder_add_string_value(builder, "美国");
+  json_builder_end_object(builder);
 
-  json_builder_set_member_name (builder, "size");
-  json_builder_begin_array (builder);
-  json_builder_add_int_value (builder, 652);
-  json_builder_add_int_value (builder, 242);
-  json_builder_end_array (builder);
+  json_builder_set_member_name(builder, "size");
+  json_builder_begin_array(builder);
+  json_builder_add_int_value(builder, 652);
+  json_builder_add_int_value(builder, 242);
+  json_builder_end_array(builder);
 
-  json_builder_end_object (builder);
+  json_builder_end_object(builder);
 
-  JsonGenerator *gen = json_generator_new ();
-  JsonNode * root = json_builder_get_root (builder);
-  json_generator_set_root (gen, root);
-  gchar *str = json_generator_to_data (gen, NULL);
+  JsonGenerator *gen = json_generator_new();
+  JsonNode *root = json_builder_get_root(builder);
+  json_generator_set_root(gen, root);
+  gchar *str = json_generator_to_data(gen, NULL);
 
-  json_node_free (root);
-  g_object_unref (gen);
-  g_object_unref (builder);
+  json_node_free(root);
+  g_object_unref(gen);
+  g_object_unref(builder);
 
   printf("%s\n", str);
 
--- odbc/base.c
+++ odbc/base.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <mysql.h>
 #include <string.h>
+#include <glib-object.h>
+#include <json-glib/json-glib.h>
+#include <ctype.h>
 
 int main(int argc, char **argv)
 {
+  // mysql initial
   MYSQL *mysql = mysql_init(NULL);
+  MYSQL_RES *res;
+  MYSQL_ROW row;
+  MYSQL_FIELD *fields;
   const char *host = "127.0.0.1";
   const char *user = "root";
   const char *passwd = "root";
@@ -13,7 +20,16 @@ int main(int argc, char **argv)
   unsigned int port = 3306;
   const char *unix_socket = "/tmp/mysql.sock";
   unsigned long clientflag = 0;
-  if (mysql_real_connect(
+  unsigned int field_count;
+  
+  int i;
+
+  // json builder initial
+  JsonBuilder *builder = json_builder_new();
+
+  
+  // connect mysql
+  if (!mysql_real_connect(
 			 mysql,
 			 host,
 			 user,
@@ -23,16 +39,63 @@ int main(int argc, char **argv)
 			 unix_socket,
 			 clientflag
 			 )) {
-    printf("CONN OK!\n");
-  } else {
-    printf("ERROR: %s\n", mysql_error(mysql));
-  }
-
-  const char *query = "CREATE TABLE odbct (id int, name varchar(20))";
-  if (mysql_real_query(mysql, query, strlen(query))) {
   	printf("%s\n", mysql_error(mysql));
   }
 
+  /*
+  char *create = "CREATE TABLE IF NOT EXISTS odbct (id int, name varchar(20))";
+  if (!mysql_real_query(mysql, create, strlen(create))) {
+  	printf("%s\n", mysql_error(mysql));
+  }
+  */
+
+  char *select = "SELECT * FROM odbct";
+  mysql_real_query(mysql, select, strlen(select));
+  res = mysql_store_result(mysql);
+  int rows = (int) mysql_num_rows(res);
+  printf("rows: %d\n", rows);
+
+  // add rows
+  json_builder_begin_object(builder);
+  json_builder_set_member_name(builder, "count");
+  json_builder_add_int_value(builder, rows);
+
+  field_count = mysql_field_count(mysql);
+  json_builder_set_member_name(builder, "fields");
+  json_builder_begin_array(builder);
+  fields = mysql_fetch_fields(res);
+  for (i = 0; i < field_count; i++) {
+    json_builder_add_string_value(builder, fields[i].name);
+  }
+  json_builder_end_array(builder);
+  
+  json_builder_set_member_name(builder, "rows");
+  json_builder_begin_object(builder);
+  while (row = mysql_fetch_row(res)) {
+    i = 0;
+    json_builder_begin_array(builder);
+    for (i; i < field_count; i++) {
+      json_builder_add_string_value(builder, row[i]);
+    }
+    json_builder_end_array(builder);
+  }
+  json_builder_end_object(builder);
+
+  json_builder_end_object(builder);
+
+  JsonGenerator *gen = json_generator_new();
+  JsonNode * root = json_builder_get_root(builder);
+  json_generator_set_root(gen, root);
+  gchar *str = json_generator_to_data(gen, NULL);
+  
+  mysql_free_result(res);
+  mysql_close(mysql);
+  json_node_free (root);
+  g_object_unref (gen);
+  g_object_unref (builder);
+
+  printf("%s\n", str);
+  
   return 0;
 }
   
